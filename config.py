c1dc4bbaa55a960c8487a75e1d88477d3e28b8cb
Matheus Emanoel Rodrigues De Albuquerque <lab.emanoel@gmail.com>
2025-05-28 09:40:39 -0300

    UPDATED: config.py - App Port, database connection

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def configure_app(app): 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['HOST'] = '0.0.0.0'
-    app.config['PORT'] = 5000
+    app.config['PORT'] = 5002
     app.config['DEBUG'] = True
+    print("Banco de dados:", app.config['SQLALCHEMY_DATABASE_URI'])
